97b8629fd327ccaedb48e2ab508e982f56f56207
akshitkrnagpal <akshitkrnagpal@gmail.com>
2020-07-07 13:37:40 +0530

    Cleaned up and added basic layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router, useParams } from '@reach/router';
+import React, { useEffect, useRef } from 'react';
+import { DOMAIN } from '../../constants/config';
 
-function App() {
+import JitsiMeetExternalAPI from '../../external_api';
+
+const Room = () => {
+  const container = useRef();
+  const { roomName } = useParams();
+
+  useEffect(() => {
+    const api = new JitsiMeetExternalAPI(DOMAIN, {
+      parentNode: container.current,
+      roomName,
+      configOverwrite: {
+        startWithAudioMuted: true,
+        startWithVideoMuted: true,
+      },
+      interfaceConfigOverwrite: {
+        DEFAULT_LOGO_URL: '',
+        DISABLE_DOMINANT_SPEAKER_INDICATOR: true,
+        DISABLE_FOCUS_INDICATOR: true,
+        SHOW_JITSI_WATERMARK: false,
+        SHOW_WATERMARK_FOR_GUESTS: false,
+        TOOLBAR_ALWAYS_VISIBLE: true,
+        VERTICAL_FILMSTRIP: false,
+        filmStripOnly: true,
+        REMOTE_THUMBNAIL_RATIO: 16 / 9,
+        HIDE_INVITE_MORE_HEADER: true,
+      },
+    });
+
+    api.on('videoConferenceJoined', console.log);
+    api.on('readyToClose', console.log);
+    api.on('suspendDetected', console.log);
+  }, [roomName]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col h-screen w-screen bg-conference'>
+      <div className='flex-1 px-8 py-8'>
+        <canvas className='w-full h-full bg-white rounded'></canvas>
+      </div>
+      <div className='overflow-hidden' style={{ height: 175 }}>
+        <div
+          ref={container}
+          style={{ transform: 'translateY(-95px)', height: 175 + 95 }}
+        />
+      </div>
     </div>
   );
-}
+};
+
+const App = () => (
+  <Router>
+    <Room path=':roomName' />
+  </Router>
+);
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,8 @@ module.exports = {
       black: '#000',
       white: '#fff',
 
+      conference: '#474747',
+
       gray: {
         100: '#f7fafc',
         200: '#edf2f7',
