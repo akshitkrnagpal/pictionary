a30e6362a19fd25a480b24667c42d69d4da907db
akshitkrnagpal <akshitkrnagpal@gmail.com>
2020-07-13 02:02:09 +0530

    Disable noisy mic detection

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,7 @@ const Room = () => {
           startWithVideoMuted: true,
           prejoinPageEnabled: false,
           openBridgeChannel: 'datachannel',
+          enableNoisyMicDetection: false,
         },
         interfaceConfigOverwrite: {
           DEFAULT_LOGO_URL: '',
