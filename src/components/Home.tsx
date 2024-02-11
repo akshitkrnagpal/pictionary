779ce645a3b6e980a867d7cf8e0665dd82efd43f
akshitkrnagpal <akshitkrnagpal@gmail.com>
2024-02-11 12:37:31 +0530

    Add join room form to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,23 @@
-import { useSocket } from '@/hooks/useSocket';
+import { useNavigate } from '@tanstack/react-router';
 
 const Home = () => {
-  const { isConnected } = useSocket();
+  const navigate = useNavigate({ from: '/' });
 
-  return <div>{isConnected ? 'Connected' : 'Not connected'}</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const room = formData.get('room') as string;
+    navigate({ to: '/$roomName', params: { roomName: room } });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='room' />
+        <button type='submit'>Join</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
