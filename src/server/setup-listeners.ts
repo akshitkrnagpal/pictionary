f5b1d12ea96d39046ee74e8b2710b073d6175e1c
akshitkrnagpal <akshitkrnagpal@gmail.com>
2024-02-11 14:52:01 +0530

    Remove console logs

--- src/server/setup-listeners.ts
+++ src/server/setup-listeners.ts
@@ -2,19 +2,11 @@ import type { Server } from 'socket.io';
 
 export const setupListeners = (io: Server) => {
   io.on('connection', (socket) => {
-    console.log('Socket connected:', socket.id);
-
-    socket.on('disconnect', () => {
-      console.log('Socket disconnected:', socket.id);
-    });
-
     socket.on('join', ({ roomName }) => {
       socket.join(roomName);
-      console.log('Socket joined room:', roomName);
     });
 
     socket.on('draw', (data) => {
-      console.log('Socket emitted draw:', data);
       socket.to(data.roomName).emit('draw', data.change);
     });
   });
